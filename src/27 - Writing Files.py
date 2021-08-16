7f83246a7ad98b37efbb9bccabcb6794f60995e9
Michael <santana11281@gmail.com>
2021-08-15 22:18:14 -0400

    write files

--- src/27 - Writing Files.py
+++ src/27 - Writing Files.py
@@ -1,4 +1,13 @@
-with open('fileTest.txt', "w") as writeFile:
-    writeFile.write("write test")
+# write
+with open('fileTest.txt', "a") as writeFile:
+    writeFile.write("write test \n")
     writeFile.write("write test 2")
 
+# amend
+# with open('fileTest.txt', "a") as writeFile:
+#     writeFile.write("write test \n")
+#     writeFile.write("write test 2")
+
+
+openFile = open('fileTest.txt')
+print(list(line for line in openFile))
