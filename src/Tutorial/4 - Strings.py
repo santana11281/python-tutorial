24b14ec6ebb10d15912e62a22c1c83026eb19f96
Michael <santana11281@gmail.com>
2021-09-05 21:42:20 -0400

    data structure

--- src/Tutorial/4 - Strings.py
+++ src/Tutorial/4 - Strings.py
@@ -1,12 +1,13 @@
-
 string = "hello world"
 printIndex = "abcde"
 exclude = 'abcdef'
 uppecase = 'ambar'
-splitSrt ='Michael, Ambar, Ninja'
+splitSrt = 'Michael, Ambar, Ninja'
 
 print(string + " my name is michael" + ' a developer')
 print(printIndex[1])
 print(exclude[0:3])
 print(uppecase.upper())
 print(splitSrt.split(','))
+
+print("mmg")
